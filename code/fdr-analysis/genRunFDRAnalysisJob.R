1700a199f9e378aef638cc3cad1af3274727e095
Jovana Maksimovic <jovana.maksimovic@petermac.org>
2021-02-26 10:32:39 +1100

    Updated code for FDR analysis to use ebGSEA functions from github.

--- code/fdr-analysis/genRunFDRAnalysisJob.R
+++ code/fdr-analysis/genRunFDRAnalysisJob.R
@@ -9,13 +9,14 @@ inputs <- list(KIRC = here::here("data/datasets/TCGA.KIRC.rds"),
 if (!dir.exists(outDir)) dir.create(outDir)
 if (!dir.exists(jobDir)) dir.create(jobDir)
 
-library(ChAMP)
-library(org.Hs.eg.db)
-library(AnnotationDbi)
-
 broadFile <- here::here("output/FDR-analysis/BROAD-sets.rds")
 
 if (!file.exists(broadFile)){
+
+    library(ChAMP)
+    library(org.Hs.eg.db)
+    library(AnnotationDbi)
+
     data("PathwayList")
     k <- keys(org.Hs.eg.db, keytype = "SYMBOL")
     keep <- sapply(PathwayList,function(x) any(x %in% k))
@@ -41,21 +42,22 @@ for (sampleType in names(inputs)){
     for (sampleNo in sampleNos) {
 
         # Start writing to this file
-        jobFile <- glue::glue("{jobDir}/FDR.{sampleNo}.job")
+        jobFile <- glue::glue("{jobDir}/FDR.{sampleType}.{sampleNo}.job")
         sink(file = jobFile)
 
         # the basic job submission script is a bash script
         cat("#!/bin/bash\n")
-        cat(glue::glue("#SBATCH --job-name=FDR.{sampleNo}.job"), "\n")
-        cat(glue::glue("#SBATCH --output={outDir}/FDR.{sampleNo}.out"), "\n")
-        cat(glue::glue("#SBATCH --error={outDir}/FDR.{sampleNo}.err"), "\n")
+        cat(glue::glue("#SBATCH --job-name=FDR.{sampleType}.{sampleNo}.job"), "\n")
+        cat(glue::glue("#SBATCH --output={outDir}/FDR.{sampleType}.{sampleNo}.out"), "\n")
+        cat(glue::glue("#SBATCH --error={outDir}/FDR.{sampleType}.{sampleNo}.err"), "\n")
         cat("#SBATCH --time=4:00:00\n")
-        cat("#SBATCH --mem=16384\n")
+        cat("#SBATCH --mem=24576\n")
         cat("#SBATCH --array=1-100\n")
         cat("#\n")
-        cat("module load R/3.6.0\n")
+        #cat("module load R/3.6.0\n")
+        cat("module load R/4.0.2\n")
         cat("#\n")
-        cat(glue::glue("Rscript {dir}/runFDRAnalysis.R {sampleNo} $SLURM_ARRAY_TASK_ID {input} {broadFile} {outDir} {sampleType}"),
+        cat(glue::glue("Rscript {dir}/fdr-analysis/runFDRAnalysis.R {sampleNo} $SLURM_ARRAY_TASK_ID {inputs[[sampleType]]} {broadFile} {outDir} {sampleType}"),
             "\n")
 
         # Close the sink!
