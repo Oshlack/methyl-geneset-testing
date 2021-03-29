b33d804ef53140a2f145ec4d6696b1352253e3b3
JovMaksimovic <jovana.maksimovic@petermac.org>
2021-03-29 21:15:14 +1100

    sra filename bug fix

--- code/salmon-quant-SRP217468.sh
+++ code/salmon-quant-SRP217468.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-for fn in data/datasets/SRP217468/SRR99*;
-do
-samp=`basename ${fn}`
-echo "Converting sample ${samp}"
-fastq-dump -I --split-files -O ${fn} ${fn}/${samp}.sra
 
-echo "Quantifying sample ${samp}"
-salmon quant -i ~/Work/genomes/hg19/salmon_v1.2.1_index -l A \
-          -1 ${fn}/${samp}_1.fastq \
-          -2 ${fn}/${samp}_2.fastq \
-          -p 6 --validateMappings -o data/datasets/SRP217468/quants/${samp}_quant
+for fn in data/datasets/SRP217468/SRR99049*/*.sra;
+    do
+        samp=`basename ${fn} .sra`
+        dir=`dirname ${fn}`
+        FILE="data/datasets/SRP217468/quants/${samp}_quant/quant.sf"
+        echo "Checking file ${FILE}"
+
+        if [ ! -f "$FILE" ]; then
+            echo "Converting sample ${samp}"
+            fastq-dump -I --split-files -O ${dir} ${fn}
+
+            echo "Quantifying sample ${samp}"
+            salmon quant -i ~/Work/genomes/hg19/salmon_v1.2.1_index -l A \
+            -1 ${dir}/${samp}_1.fastq \
+            -2 ${dir}/${samp}_2.fastq \
+            -p 6 --validateMappings -o data/datasets/SRP217468/quants/${samp}_quant
+        fi
 done
